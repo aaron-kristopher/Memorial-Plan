9e41ee4bcebf1e063d4ad344f2113cf6df0dcdb6
aaron-kristopher <aaron.lim.cstr@gmail.com>
2023-03-29 12:44:28 +0800

    included installment plan page and modified index

--- js/script.js
+++ js/script.js
@@ -28,3 +28,69 @@ var swiper = new Swiper(".slide-content", {
         }
     },
   });
+
+const regularButton = document.getElementById('regular-button');
+const innerWalkButton = document.getElementById('inner-walk-button');
+const walkButton = document.getElementById('walk-button');
+const innerDriveButton = document.getElementById('inner-drive-button');
+const driveButton = document.getElementById('drive-button');
+const installmentHeader = document.getElementById('installment-header');
+
+const regularSlide = document.getElementById('installments-regular-slides');
+const innerWalkSlide = document.getElementById('installments-inner-walk-slide');
+const walkSlide = document.getElementById('installments-walk-slide');
+const innerDriveSlide = document.getElementById('installments-inner-drive-slide');
+const driveSlide = document.getElementById('installments-drive-slide');
+
+regularButton.addEventListener('click', () => {
+  installmentHeader.textContent = 'REGULAR PLANS';
+  regularSlide.style.display = 'block';
+
+  innerWalkSlide.style.display = 'none';
+  walkSlide.style.display = 'none';
+  innerDriveSlide.style.display = 'none'
+  driveSlide.style.display = 'none';
+});
+
+innerWalkButton.addEventListener('click', () => {
+  installmentHeader.textContent = 'INNER WALK PLANS';
+  innerWalkSlide.style.display = 'block';
+
+  regularSlide.style.display = 'none';
+  walkSlide.style.display = 'none';
+  innerDriveSlide.style.display = 'none';
+  driveSlide.style.display = 'none';
+});
+
+walkButton.addEventListener('click', () => {
+  installmentHeader.textContent = 'WALK PLANS';
+  walkSlide.style.display = 'block';
+
+  regularSlide.style.display = 'none';
+  innerWalkSlide.style.display = 'none';
+  innerDriveSlide.style.display = 'none';
+  driveSlide.style.display = 'none';
+});
+
+innerDriveButton.addEventListener('click', () => {
+  installmentHeader.textContent = 'INNER DRIVE PLANS';
+  innerDriveSlide.style.display = 'block';
+
+  regularSlide.style.display = 'none';
+  innerWalkSlide.style.display = 'none';
+  walkSlide.style.display = 'none';
+  driveSlide.style.display = 'none';
+});
+
+driveButton.addEventListener('click', () => {
+  installmentHeader.textContent = 'DRIVE PLANS';
+  driveSlide.style.display = 'block';
+
+  regularSlide.style.display = 'none';
+  innerWalkSlide.style.display = 'none';
+  walkSlide.style.display = 'none';
+  innerDriveSlide.style.display = 'none';
+});
+
+
+
